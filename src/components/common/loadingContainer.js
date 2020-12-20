3e9ae4d4a7d7b10accc14cb6803af1d08ae411fb
Paul Mardling <paulm@connecterra.io>
2020-12-20 12:09:59 +0100

    Style loadingContainer

--- src/components/common/loadingContainer.js
+++ src/components/common/loadingContainer.js
@@ -1,18 +1,25 @@
 import React from "react";
-import { CircularProgress, Container } from "@material-ui/core";
+import { Box, CircularProgress, Container, Typography } from "@material-ui/core";
 
 import { ErrorBoundary } from "../../components";
 
 const LoadingContainer = ({ component: Component, data, error, loading }) =>
-  <Container className="loadingContainer">
+  <Container className="loadingContainer" display="flex">
     {console.log("error: ", error)}
-    {
-      loading
-        ? <CircularProgress/>
-        : error
-          ? <ErrorBoundary error={error} />
-          : <Component {...data} />
-    }
+    <Box className="loadingContainer-box" display="flex" flexDirection="column">
+
+      <CircularProgress />
+      <Typography className="loadingContainer-box__text" variant="h4">
+        Loading...
+      </Typography>
+    </Box>
+    {/*{*/}
+    {/*  loading*/}
+    {/*    ? <CircularProgress/>*/}
+    {/*    : error*/}
+    {/*      ? <ErrorBoundary error={error} />*/}
+    {/*      : <Component {...data} />*/}
+    {/*}*/}
 
   </Container>;
 
